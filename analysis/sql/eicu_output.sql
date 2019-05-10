413988a3a68760b779a24eeb2e6dde4b5bc2d2a4
Kimiko Huang <kimikohuang@gmail.com>
2019-05-10 15:18:06 -0400

    Update eicu_output.sql

--- analysis/sql/eicu_output.sql
+++ analysis/sql/eicu_output.sql
@@ -2,5 +2,7 @@ SELECT patientunitstayid
 , sum(cellvaluenumeric) AS sum_output
 FROM `physionet-data.eicu_crd.intakeoutput` 
 WHERE LOWER(cellpath) LIKE '%output (ml)%' 
+AND intakeOutputOffset BETWEEN -6*60 AND 6*60
 GROUP BY patientunitstayid
+ORDER BY patientunitstayid
 LIMIT 1000
