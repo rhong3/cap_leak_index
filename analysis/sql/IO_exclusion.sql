28d93e165058f5aa1789e75ae900d5e30eb14384
Kimiko Huang <kimikohuang@gmail.com>
2019-05-11 10:15:14 -0400

    Update IO_exclusion.sql

--- analysis/sql/IO_exclusion.sql
+++ analysis/sql/IO_exclusion.sql
@@ -1,6 +1,6 @@
 -- ------------------------------------------------------------------
 -- Title: Select patientunitstayid from intakeoutput 
--- Notes:  cap_leak_index/analysis/sql/IO_exclusion.sql 
+-- Notes: cap_leak_index/analysis/sql/IO_exclusion.sql 
 --        cap_leak_index, 20190511 NYU Datathon
 --        eICU Collaborative Research Database v2.0.
 -- ------------------------------------------------------------------
