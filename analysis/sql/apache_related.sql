9be6a34350fdbb47c5da6ddc3f75d097bfc5430e
Kimiko Huang <kimikohuang@gmail.com>
2019-05-11 10:14:56 -0400

    Update apache_related.sql

--- analysis/sql/apache_related.sql
+++ analysis/sql/apache_related.sql
@@ -1,3 +1,9 @@
+-- ------------------------------------------------------------------
+-- Title: Select patients from apachepatientresult 
+-- Notes: cap_leak_index/analysis/sql/apache_related.sql
+--        cap_leak_index, 20190511 NYU Datathon
+--        eICU Collaborative Research Database v2.0.
+-- ------------------------------------------------------------------
 SELECT patientunitstayid, apachescore, actualicumortality, 
 actualhospitalmortality, unabridgedunitlos, unabridgedhosplos, unabridgedactualventdays 
 FROM `physionet-data.eicu_crd.apachepatientresult`
