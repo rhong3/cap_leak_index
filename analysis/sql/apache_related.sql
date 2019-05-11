16cb56a4470f759cf9050d04f3e9c3f3d65bfc7c
theonesp <marmengo@bidmc.harvard.edu>
2019-05-11 10:07:09 -0400

    body surface area

---
+++ analysis/sql/apache_related.sql
@@ -0,0 +1,3 @@
+SELECT patientunitstayid, apachescore, actualicumortality, 
+actualhospitalmortality, unabridgedunitlos, unabridgedhosplos, unabridgedactualventdays 
+FROM `physionet-data.eicu_crd.apachepatientresult`
